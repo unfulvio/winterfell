03f2c94775b003167d80d19ac0c09ef0b3ba1279
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2018-04-27 12:18:25 -0700

    Use localhost for redis connection

--- main.go
+++ main.go
@@ -7,13 +7,16 @@ import (
 	"net/http"
 	"time"
 	"strings"
+	"fmt"
 )
 
 func main() {
 
+	fmt.Println( "-- Starting Wintefell --" )
+
 	router := gin.Default()
 	pool   := &redis.Pool{ Dial: func() (redis.Conn, error) {
-		c, err := redis.Dial("tcp", ":6379")
+		c, err := redis.Dial("tcp", "127.0.0.1:6379")
 		if err != nil {
 			return nil, err
 		}
@@ -23,17 +26,32 @@ func main() {
 	// get orders for a given shop
 	router.POST("v2/shops/:shop_uuid/orders", func(c *gin.Context) {
 
+		shopUUID := c.Param("shop_uuid")
+
+		// return server error if pool errored out
+		if pool == nil {
+			c.JSON(500, gin.H{ "status" : 500 })
+		}
+
+		fmt.Printf( "Requesting orders for shop %s...\n", shopUUID )
+
 		// grab the request data to be sent to Redis
-		shopUUID      := c.Param("shop_uuid")
-		authString    := c.Request.Header.Get( "authorization" )
-		auth          := strings.SplitN(strings.Replace(authString, "Token ", "", 1), ",", 2)
-		params        := []interface{}{auth, "params_hash_for_worker"}
+		authString := c.Request.Header.Get( "authorization" )
+		auth       := strings.SplitN(strings.Replace(authString, "Token ", "", 1), ",", 2)
+		params     := []interface{}{auth, "params_hash_for_worker"}
+
+		fmt.Println( "Sending new task to Redis..." )
 
 		// prepare and enqueue job to Redis
 		job := gokiq.NewJob("ApiOrderEventWorker", "default", params, 1)
 
-		job.Enqueue(pool)
+		jobID := job.Enqueue(pool)
+
+		fmt.Printf( "Got Job ID %s\n", jobID )
+
 		job.EnqueueAt(time.Now(), pool)
+
+		fmt.Println( "Done!" )
 
 		// tell the requester that everything's gonna be ok
 		c.JSON(http.StatusOK, gin.H{
